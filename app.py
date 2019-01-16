50a91ea48a8c20f01d4c14a29c4d20478562a2ce
Du Lam <u103034@DBG--C02XH0DWJG5K--Du-Lam.local>
2019-01-16 06:59:36 -0800

    First commit, added REST API to access items, stores, and user authentication.

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
+
+from security import authenticate, identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+#200 success status code 
+#201 success in create an object 
+#202 success in creating an object but there's a delay
+#400 bad request client fault
+#500 internal server error
+#CRUDE create read update delete
+# @app.route('/student/<string:name>')
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'jose'
+#Api class allows us to work easily w/ Resource
+#Resource has to be a class 
+api = Api(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+jwt = JWT(app, authenticate, identity) # /auth
+
+
+api.add_resource(Store, '/store/<string:name>')
+api.add_resource(StoreList, '/stores')
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
+
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, debug=True)
